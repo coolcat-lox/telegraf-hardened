e9d43d6da851e4d02a4318f91b0ad6aa8630e2db
CooL-CaT <neonchel2288@gmail.com>
2026-04-23 18:05:39 +0300

    fix: mutation error

--- src/core/network/client.ts
+++ src/core/network/client.ts
@@ -424,11 +424,13 @@ async function answerToWebhook(
 }
 
 function redactToken(error: Error): never {
-    error.message = error.message.replace(
-        /\/(bot|user)(\d+):[^/]+\//,
-        '/$1$2:[REDACTED]/'
-    )
-    throw error
+  const message = error.message.replace(
+    /\/(bot|user)(\d+):[^/]+\//,
+    '/$1$2:[REDACTED]/'
+  )
+  const newError = new Error(message)
+  newError.stack = error.stack
+  throw newError
 }
 
 type Response = http.ServerResponse
